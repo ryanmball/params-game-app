02f6124cd30be4664376c9a34c08a680d9db52de
ryan <ball.ryan.rb@gmail.com>
2021-05-06 12:53:17 -0600

    adds number_guesser route and games controller method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,25 @@
 class GamesController < ApplicationController
   def user_name
     user_name = params[:user_name]
-    render json: {message: "Your name is #{user_name.upcase}"} 
+    if user_name[0] == "a"
+      render json: {
+        message1: "Your name is #{user_name.upcase}",
+        message2: "Hey, your name starts with the first letter of the alphabet!"
+      }
+    else
+      render json: {message: "Your name is #{user_name.upcase}"}
+    end
+  end
+
+  def number_guesser
+    number = 36
+    user_guess = params[:user_guess].to_i
+    if user_guess > number
+      render json: {message: "Too high! Guess again."}
+    elsif user_guess < number
+      render json: {message: "Too low! Guess again."}
+    elsif user_guess == number
+      render json: {message: "That is correct! The number is #{number}."}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "user_name" => "games#user_name"
+
+  get "number_guesser" => "games#number_guesser"
 end
